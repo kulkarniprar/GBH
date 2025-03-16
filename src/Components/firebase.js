dcff700a815f95b450cb252091f0740236c2e095
kulkarniprar <151203739+kulkarniprar@users.noreply.github.com>
2025-03-16 11:18:01 +0530

    Update firebase.js

--- src/Components/firebase.js
+++ src/Components/firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyC5Aof7TfdxfK7zfYnc91mNKk3B88YRE40",
-  authDomain: "gbhack-70f04.firebaseapp.com",
-  projectId: "gbhack-70f04",
-  storageBucket: "gbhack-70f04.firebasestorage.app",
-  messagingSenderId: "460109800878",
-  appId: "1:460109800878:web:d6a81210485ad12664943f",
-  measurementId: "G-4CTNFQ68C6"
+ //API Keys
 };
 
 // Initialize Firebase
